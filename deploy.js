d550fe69b76daf77bcc0452d8eba0bf3c85ed7f1
Rebecca <47248246+rebeccawilliamss@users.noreply.github.com>
2022-09-23 11:44:13 +0100

    Add files via upload

---
+++ deploy.js
@@ -0,0 +1,40 @@
+const main = async () => {
+
+    // compile contract and generate files in artifacts folder
+    const nftContractFactory = await hre.ethers.getContractFactory('MyEpicNFT');
+
+    // create a local eth network
+    const nftContract = await nftContractFactory.deploy();
+
+    // wait until contract is deployed
+    await nftContract.deployed();
+
+    // gives us the address of the deployed contract
+    console.log("Contract deployed to:", nftContract.address);
+
+    // Call the function.
+    let txn = await nftContract.makeAnEpicNFT()
+
+    // Wait for NFT to be mined.
+    await txn.wait()
+    console.log("Minted NFT #1")
+
+    // Mint another NFT for fun.
+    txn = await nftContract.makeAnEpicNFT()
+
+    // Wait for it to be mined.
+    await txn.wait()
+    console.log("Minted NFT #2")
+  };
+
+  const runMain = async () => {
+    try {
+      await main();
+      process.exit(0);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
+  };
+
+  runMain();
